7a49e09082c2891645b58083ce287867dde5c2a9
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-04-04 08:12:48 -0400

    Minor edit to theme

--- zsh/themes/minimal.zsh
+++ zsh/themes/minimal.zsh
@@ -3,4 +3,4 @@ ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg[white]%}] "
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%} o%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%} x%{$reset_color%}"
 
-PROMPT='%2~ $(git_prompt_info)»%b '
+PROMPT='%2~ $(git_prompt_info)>%b '
